0eefad1e97e733191f8effc4298c4a1333a22d7f
Nicolas Dao <nicolas.dao@gmail.com>
2018-11-12 16:09:32 +1100

    feat: Add support for formatting an object to a specific table schema

--- src/format.js
+++ src/format.js
@@ -33,6 +33,168 @@ const cleanData = (obj) => {
 	}, {})
 }
 
+const _allowedSchemaTypes = { 'string': true, 'number': true, 'boolean': true, 'object': true }
+const fitToSchema = (obj={}, schema={}, options={}) => {
+	const keys = Object.keys(schema)
+	if (keys.length == 0)
+		return {}
+	
+	return keys.reduce((acc,key) => {
+		const v = obj[key]
+		const schemaType = schema[key]
+		let sType
+		const schemaTypeIsArray = Array.isArray(schemaType)
+		const schemaTypeIsObject = typeof(schemaType) == 'object'
+		if (schemaTypeIsArray) {
+			if (!schemaType[0])
+				throw new Error(`Invalid type. Field '${key}' is an array of empty type (ref. #01).`)
+
+			const t = typeof(schemaType[0]) == 'object' ? 'object' : schemaType[0]
+			sType = `${t}`.toLowerCase()
+			if (!_allowedSchemaTypes[sType])
+				throw new Error(`Field '${key}' uses an unsupported type ${sType}. Only allowed types are: ${Object.keys(_allowedSchemaTypes)} (ref. #02)`)
+
+			sType = `array-${sType}`
+		} else {
+			sType = schemaTypeIsObject ? 'object' : `${schemaType}`.toLowerCase()
+			if (!_allowedSchemaTypes[sType])
+				throw new Error(`Field '${key}' uses an unsupported type ${sType}. Only allowed types are: ${Object.keys(_allowedSchemaTypes)} (ref. #03)`)
+		}
+
+		const fieldType = typeof(v)
+		const fieldIsArray = Array.isArray(v)
+		const fieldIsDate = v instanceof Date
+		const fieldIsObject = fieldType == 'object'
+
+		if ((v || v === 0 || v === '' || v === false) && fieldType == sType && !fieldIsArray && !fieldIsDate && !fieldIsObject)
+			acc[key] = v === '' ? 'NULL' : v
+		else if (!v) {
+			if (sType == 'string') {
+				acc[key] = 'NULL'
+			} else if (sType == 'number') {
+				acc[key] = -1
+			} else if (sType == 'boolean') {
+				acc[key] = false
+			} else if (schemaTypeIsArray) {
+				const schemaArrayType = schemaType[0]
+				if (sType == 'array-string')
+					acc[key] = ['NULL']
+				else if (sType == 'array-number')
+					acc[key] = [-1]
+				else if (sType == 'array-boolean')
+					acc[key] = [false]
+				else if (sType == 'array-object')
+					acc[key] = [fitToSchema({}, schemaArrayType, { isNull: true })]
+				else
+					throw new Error(`Field '${key}' uses an unsupported type ${sType} (ref. #04).`)
+			} else if (sType == 'object') 
+				acc[key] = fitToSchema({}, schemaType, { isNull: true })
+			else
+				throw new Error(`Field '${key}' uses an unsupported type ${sType} (ref. #05).`)
+		}
+		else {
+			if (sType == 'string') {
+				const newVal = 
+					fieldIsDate ? v.toISOString() : 
+						fieldIsObject ? 'Object' : `${v}`
+				acc[key] = newVal
+			} else if (sType == 'number') {
+				const newVal = 
+					fieldType == 'string' && /^[0-9]+$/.test(v) ? v*1 : 
+						fieldType == 'boolean' ? (v ? 1 : 0) : -1
+				fieldIsDate ? v.getTime() : -1
+
+				acc[key] = newVal
+			} else if (sType == 'boolean') {
+				const newVal = 
+					fieldType == 'string' ? (v ? true : false) : 
+						fieldType == 'number' ? (v ? true : false) : 
+							fieldIsArray || fieldIsObject || fieldIsDate ? true : false
+
+				acc[key] = newVal
+			} else if (schemaTypeIsArray) {
+				const schemaArrayType = schemaType[0]
+				const newVal = 
+					fieldType == 'string' && sType == 'array-string' ? [v] : 
+						fieldType == 'string' && sType == 'array-number' ? (/^[0-9]+$/.test(v) ? [v*1] : [-1]) :  
+							fieldType == 'string' && sType == 'array-boolean' ? [true] : 
+								fieldType == 'string' && sType == 'array-object' ? [fitToSchema({}, schemaArrayType, { isNull: true })] : 
+									fieldType == 'number' && sType == 'array-string' ? [`${v}`] : 
+										fieldType == 'number' && sType == 'array-number' ? [v] :  
+											fieldType == 'number' && sType == 'array-boolean' ? (v ? [true] : [false]) : 
+												fieldType == 'number' && sType == 'array-object' ? [fitToSchema({}, schemaArrayType, { isNull: true })] : 
+													fieldType == 'boolean' && sType == 'array-string' ? (v ? ['true'] : ['false']) : 
+														fieldType == 'boolean' && sType == 'array-number' ? (v ? [1] : [0]) :  
+															fieldType == 'boolean' && sType == 'array-boolean' ? (v ? [true] : [false]) : 
+																fieldType == 'boolean' && sType == 'array-object' ? [fitToSchema({}, schemaArrayType, { isNull: true })] : 
+																	fieldIsDate && sType == 'array-string' ? [v.toISOString()] : 
+																		fieldIsDate && sType == 'array-number' ? [v.getTime()] :  
+																			fieldIsDate && sType == 'array-boolean' ? [true] : 
+																				fieldIsDate && sType == 'array-object' ? [fitToSchema({}, schemaArrayType, { isNull: true })] : 
+																					fieldIsArray ? _getNewArrayVal(v,sType,schemaArrayType,key) : 
+																						fieldType == 'object' && sType == 'array-string' ? ['NULL'] : 
+																							fieldType == 'object' && sType == 'array-number' ? [-1] :  
+																								fieldType == 'object' && sType == 'array-boolean' ? [false] : 
+																									fieldType == 'object' && sType == 'array-object' ? [fitToSchema(v, schemaArrayType, { isNull: false })] : null 
+
+				if (!newVal)
+					throw new Error(`Field '${key}' uses an unsupported type ${sType} (ref. #06).`)
+
+				acc[key] = newVal
+			} else if (sType == 'object') {
+				const newVal = !fieldIsArray && fieldIsObject ? v : {}
+				const opts = Object.keys(newVal).some(x => x) ? { isNull: false } : { isNull: true }
+				acc[key] = fitToSchema(newVal, schemaType, opts)
+			}
+			else
+				throw new Error(`Field '${key}' uses an unsupported type ${sType} (ref. #07).`)
+		}
+
+		return acc
+	}, options.isNull === undefined ? {} : { _isNull: options.isNull ? true : false })
+}
+
+const _getNewArrayVal = (arrayVal=[], sType, schemaType, key) => {
+	const a = arrayVal.some(x => x) ? arrayVal : ['NULL']
+	return a.map(x => {
+		const v = 
+			!x && typeof(x) == 'string' ? 'NULL' : 
+				!x && typeof(x) == 'number' ? 0 : 
+					!x ? 'NULL' : x
+
+		const fieldType = typeof(v)
+		const fieldIsDate = v instanceof Date
+
+		const newVal = 
+			fieldType == 'string' && sType == 'array-string' ? v : 
+				fieldType == 'string' && sType == 'array-number' ? (/^[0-9]+$/.test(v) ? (v*1) : -1) :  
+					fieldType == 'string' && sType == 'array-boolean' ? true : 
+						fieldType == 'string' && sType == 'array-object' ? fitToSchema({}, schemaType, { isNull: true }) : 
+							fieldType == 'number' && sType == 'array-string' ? `${v}` : 
+								fieldType == 'number' && sType == 'array-number' ? v :  
+									fieldType == 'number' && sType == 'array-boolean' ? (v ? true : false) : 
+										fieldType == 'number' && sType == 'array-object' ? fitToSchema({}, schemaType, { isNull: true }) : 
+											fieldType == 'boolean' && sType == 'array-string' ? (v ? 'true' : 'false') : 
+												fieldType == 'boolean' && sType == 'array-number' ? (v ? 1 : 0) :  
+													fieldType == 'boolean' && sType == 'array-boolean' ? (v ? true : false) : 
+														fieldType == 'boolean' && sType == 'array-object' ? fitToSchema({}, schemaType, { isNull: true }) : 
+															fieldIsDate && sType == 'array-string' ? v.toISOString() : 
+																fieldIsDate && sType == 'array-number' ? v.getTime() :  
+																	fieldIsDate && sType == 'array-boolean' ? true : 
+																		fieldIsDate && sType == 'array-object' ? fitToSchema({}, schemaType, { isNull: true }) : 
+																			fieldType == 'object' && sType == 'array-string' ? 'NULL' : 
+																				fieldType == 'object' && sType == 'array-number' ? -1 :  
+																					fieldType == 'object' && sType == 'array-boolean' ? false : 
+																						fieldType == 'object' && sType == 'array-object' ? fitToSchema(v, schemaType, { isNull: false }) : null 
+
+		if (!newVal)
+			throw new Error(`Field '${key}' uses an unsupported type ${sType} (ref. #08).`)
+
+		return newVal
+	})
+}
+
 module.exports = {
-	cleanData
+	cleanData,
+	fitToSchema
 }
--- test/format.js
+++ test/format.js
@@ -10,9 +10,9 @@
 /* global it */
 
 const { assert } = require('chai')
-const { cleanData } = require('../src/format')
+const { cleanData, fitToSchema } = require('../src/format')
 
-describe('format', () => 
+describe('format', () => {
 	describe('#cleanData', () => {
 		it('Should rename all number properties.', () => {
 			const d = new Date()
@@ -55,4 +55,125 @@ describe('format', () =>
 			assert.equal(newObj_01.b[3][0]['#234'][0]['#432'].class, 3, '15')
 			assert.equal(newObj_01.b[3][0]['#234'][0]['#432'].age, 34, '16')
 		})
-	}))
+	})
+
+	describe('#fitToSchema', () => {
+		it('Should transform an object so it fits a specific schema', () => {
+			const schema = {
+				id: 'number',
+				firstName: 'string',
+				age: 'number',
+				address: {
+					line1: 'string',
+					line2: 'string',
+					postcode: 'string'
+				},
+				friends: [{
+					name: 'string',
+					age: 'number',
+					dob: 'string',
+					married: 'boolean'
+				}],
+				tags: ['string']
+			}
+			const obj_01 = {
+				id: '123',
+				firstName: 'Nicolas',
+				lastName: 'Dao',
+				age: 37,
+				address: {
+					line1: 'Waterloo, NSW 2017',
+					postcode: 2017
+				}
+			}
+			const newObj_01 = fitToSchema(obj_01, schema)
+			assert.equal(newObj_01.id, 123, '01')
+			assert.equal(newObj_01.firstName, 'Nicolas', '02')
+			assert.isNotOk(newObj_01.lastName, '03')
+			assert.equal(newObj_01.age, 37, '04')
+			assert.equal(newObj_01.address.line1, 'Waterloo, NSW 2017', '05')
+			assert.equal(newObj_01.address.line2, 'NULL', '06')
+			assert.equal(newObj_01.address.postcode, '2017', '07')
+			assert.equal(newObj_01.address._isNull, false, '08')
+			assert.equal(newObj_01.friends.length, 1, '09')
+			assert.equal(newObj_01.friends[0]._isNull, true, '10')
+			assert.equal(newObj_01.friends[0].name, 'NULL', '11')
+			assert.equal(newObj_01.friends[0].age, -1, '12')
+			assert.equal(newObj_01.friends[0].dob, 'NULL', '13')
+			assert.equal(newObj_01.friends[0].married, false, '14')
+			assert.equal(newObj_01.tags.length, 1, '15')
+			assert.equal(newObj_01.tags[0], 'NULL', '16')
+
+			const obj_02 = {
+				id: '123',
+				firstName: 'Nicolas',
+				lastName: 'Dao',
+				age: 37,
+				friends: [{
+					name: 'Brendan',
+					age: 30,
+					dob: new Date(1988,6,24),
+					married: 'true'
+				}, {
+					name: 'Boris'
+				}],
+				tags:['banana', 123]
+			}
+			const newObj_02 = fitToSchema(obj_02, schema)
+			assert.equal(newObj_02.id, 123, '17')
+			assert.equal(newObj_02.firstName, 'Nicolas', '18')
+			assert.isNotOk(newObj_02.lastName, '19')
+			assert.equal(newObj_02.age, 37, '20')
+			assert.equal(newObj_02.address.line1, 'NULL', '21')
+			assert.equal(newObj_02.address.line2, 'NULL', '22')
+			assert.equal(newObj_02.address.postcode, 'NULL', '23')
+			assert.equal(newObj_02.address._isNull, true, '24')
+			assert.equal(newObj_02.friends.length, 2, '25')
+			assert.equal(newObj_02.friends[0]._isNull, false, '26')
+			assert.equal(newObj_02.friends[0].name, 'Brendan', '27')
+			assert.equal(newObj_02.friends[0].age, 30, '28')
+			assert.equal(newObj_02.friends[0].dob, '1988-07-23T14:00:00.000Z', '29')
+			assert.equal(newObj_02.friends[0].married, true, '30')
+			assert.equal(newObj_02.friends[1]._isNull, false, '31')
+			assert.equal(newObj_02.friends[1].name, 'Boris', '32')
+			assert.equal(newObj_02.friends[1].age, -1, '33')
+			assert.equal(newObj_02.friends[1].dob, 'NULL', '34')
+			assert.equal(newObj_02.friends[1].married, false, '35')
+			assert.equal(newObj_02.tags.length, 2, '36')
+			assert.equal(newObj_02.tags[0], 'banana', '37')
+			assert.equal(newObj_02.tags[1], '123', '38')
+
+			const obj_03 = {
+				id: '123',
+				firstName: '',
+				lastName: 'Dao',
+				age: 0,
+				friends: {
+					name: 'Brendan',
+					age: 30,
+					dob: new Date(1988,6,24),
+					married: 'true'
+				},
+				tags:['banana', 123]
+			}
+			const newObj_03 = fitToSchema(obj_03, schema)
+			assert.equal(newObj_03.id, 123, '39')
+			assert.equal(newObj_03.firstName, 'NULL', '40')
+			assert.isNotOk(newObj_03.lastName, '41')
+			assert.equal(newObj_03.age, 0, '42')
+			assert.equal(newObj_03.address.line1, 'NULL', '43')
+			assert.equal(newObj_03.address.line2, 'NULL', '44')
+			assert.equal(newObj_03.address.postcode, 'NULL', '45')
+			assert.equal(newObj_03.address._isNull, true, '46')
+			assert.equal(newObj_03.friends.length, 1, '47')
+			assert.equal(newObj_03.friends[0]._isNull, false, '48')
+			assert.equal(newObj_03.friends[0].name, 'Brendan', '49')
+			assert.equal(newObj_03.friends[0].age, 30, '50')
+			assert.equal(newObj_03.friends[0].dob, '1988-07-23T14:00:00.000Z', '51')
+			assert.equal(newObj_03.friends[0].married, true, '52')
+			assert.equal(newObj_03.tags.length, 2, '53')
+			assert.equal(newObj_03.tags[0], 'banana', '54')
+			assert.equal(newObj_03.tags[1], '123', '55')
+		})
+	})
+})
